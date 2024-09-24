481a043fac60940ded14a8982055ecbcb4538951
Eduardo M. Rodrigues <eduardo@eduardo.rodrigues.nom.br>
2024-09-24 09:47:33 -0300

    Exception handlers

---
+++ src/main/java/edware/rest_api/service/exception/CustomExceptionHandler.java
@@ -0,0 +1,28 @@
+package edware.rest_api.service.exception;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.springframework.http.*;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        List<String> details = new ArrayList<>();
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            details.add(error.getDefaultMessage());
+        }
+        ErrorResponse error = new ErrorResponse("Validation Failed", details);
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+}
